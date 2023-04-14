58338d7125b70509c72fcb9f639a2afadfb8cc48
IuryBenicio <107493629+IuryBenicio@users.noreply.github.com>
2023-04-14 12:03:55 -0300

    Commit final

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
 const form = document.getElementById('form-AeB');
-
-const inputA = document.getElementById("A").value;
-const inputB = document.getElementById("B").value;
-
-const valorA = parseFloat(inputA);
-const valorB = parseFloat(inputB);
-
-const contMsgSucesso =  document.querySelector('.msg-sucesso');
-const contMsgErro = document.querySelector('.msg-erro');
-
-const mensagemErro = `o A que tem por valor <b> ${valorA} não é maior que o B que tem por valor ${valorB}</b> `
-const mensagemSucesso = `o A que tem o valor de <b>${valorA}</b> é maior que o valor B de <b>${valorB}</b>`;
-
 function formValido(campoA, campoB){
 
     if(campoA>campoB){
@@ -25,12 +12,25 @@ function formValido(campoA, campoB){
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(inputA + inputB);
 
+    const inputA = document.getElementById("A").value;
+    const inputB = document.getElementById("B").value;
+
+    const valorA = parseFloat(inputA);
+    const valorB = parseFloat(inputB);
+
+    const contMsgSucesso =  document.querySelector('.msg-sucesso');
+    const contMsgErro = document.querySelector('.msg-erro');
+
+    const mensagemErro = `<b> ${valorA} não é maior que <b>${valorB}</b> `
+    const mensagemSucesso = `<b>${valorA}</b> é maior que <b>${valorB}</b>`;
+
+    console.log(inputA + inputB);
     if(formValido(valorA, valorB)){
         contMsgSucesso.innerHTML = mensagemSucesso;
         contMsgSucesso.style.display = 'block';
         console.log(formValido.value + ' foi validado');
+        contMsgErro.style.display = 'none';
 
         const inputA = ' ';
         const inputB = ' ';
@@ -39,5 +39,7 @@ form.addEventListener('submit', function(e){
         contMsgErro.innerHTML = mensagemErro;
         contMsgErro.style.display = 'block';
         console.log(formValido.value + ' n foi validado');
+        contMsgSucesso.style.display = 'none';
     }
 })
+
