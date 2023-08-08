7b0635b00ff4001c864c6c5eebbcc0199c88f895
IuryBenicio <107493629+IuryBenicio@users.noreply.github.com>
2023-08-08 12:06:35 -0300

    initial_commit

---
+++ main.js
@@ -0,0 +1,18 @@
+class Aluno{
+    constructor(nomeAluno, notaAluno){
+        this.nomeAluno = nomeAluno
+        this.notaAluno = notaAluno
+    }
+}
+
+const jose = new Aluno('Jose', 8);
+const elisa = new Aluno('Elisa', 5);
+const luis = new Aluno('Luis', 6);
+const julio = new Aluno('Julio', 9);
+const regina = new Aluno('Regina', 2);
+
+let alunos = [jose, elisa, luis, elisa, julio, regina];
+
+const alunosAprovados = alunos.filter(aluno => (aluno.notaAluno >= 6))
+
+console.log(alunosAprovados)
