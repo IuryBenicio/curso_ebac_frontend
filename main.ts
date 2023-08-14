0482a31459594b1043bc94e9b767ea4adc4f5786
IuryBenicio <107493629+IuryBenicio@users.noreply.github.com>
2023-08-14 15:16:39 -0300

    initial_commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+let nome: string = 'Iury';
+
+let a: number = 3;
+
+let b: number = 5;
+
+const ola: string =  'ola meu nome é: ';
+
+let saudacao = (saudacao: string, nomeDoRapaz: string) => saudacao + nomeDoRapaz;
+
+let multiplica = (a:number , b:number) => a * b;
+
+console.log(saudacao(ola, nome))
+
+console.log(multiplica(a,b))
